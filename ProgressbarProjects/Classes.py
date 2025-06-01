4cfac4fddd91e023f1a9193b3871cc1912664196
Lunarium856 <santinopcalpo@gmail.com>
2025-06-02 01:29:21 +0800

    Create Classes.py

---
+++ ProgressbarProjects/Classes.py
@@ -0,0 +1,17 @@
+class Project:
+    def __init__(self, projectID, name, desc, time_create, time_update, tasks, progress):
+        pass
+
+    def Edit():
+        pass
+
+    def Delete():
+        pass
+
+class Task:
+    def __init__(self, taskID, name, desc, weight, subtasks):
+        pass
+
+class Subtask:
+    def __init__(self, subtaskID, name, desc, weight):
+        pass
