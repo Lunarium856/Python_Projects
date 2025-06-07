20a09a1bf0420963da1854e85cd6ba135c0fe2ce
Lunarium856 <santinopcalpo@gmail.com>
2025-06-07 23:02:07 +0800

    Window Yaaaay

--- ProgressbarProjects/Main.py
+++ ProgressbarProjects/Main.py
@@ -1,11 +1,23 @@
 from Classes import Project as p, Task as t, Subtask as st
-# from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import Qt
 import sys, json, os
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Project Manager")
+        self.setGeometry(700, 300, 500, 500) # x,y,width px,height px
+        self.setWindowIcon(QIcon("app_icon.png"))
+
+        label = QLabel("Hello", self)
+        label.setFont(QFont("Arial", 30)) # fontname, size
+        label.setGeometry(0,0,500,100)
+        label.setStyleSheet("color: #3F3F3F;"
+                            "background-color: #6fdcf7")
+        
+        label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
 
 def main():
     app = QApplication(sys.argv)
